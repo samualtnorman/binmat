a1011aedb08c17ebd23094d59e7cd5ace6879b56
Samual Norman <samualtnorman@gmail.com>
2022-03-25 22:19:37 +0000

    remove `BinmatGame` and redo `index.ts` `export`s

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,7 @@
-import createState, { Role } from "./createState"
-import doMove from "./doMove"
-import { Action, Move, StatusCode } from "./shared"
-
-export { Role } from "./createState"
-export { Action } from "./shared"
-export type { Move } from "./shared"
-
-export class BinmatGame {
-	state = createState()
-	winner: Role | undefined
-
-	play(move: Move) {
-		const result = doMove(this.state, move)
-
-		switch (result.status) {
-			case StatusCode.Ok:
-				return result.binlog
-
-			case StatusCode.AttackerWin: {
-				this.winner = Role.Attacker
-
-				return result.binlog
-			}
-
-			case StatusCode.DefenderWin: {
-				this.winner = Role.Defender
-
-				return result.binlog
-			}
-
-			default: {
-				const result = doMove(this.state, { action: Action.Pass })
-
-				switch (result.status) {
-					case StatusCode.Ok:
-						return result.binlog
-
-					case StatusCode.DefenderWin: {
-						this.winner = Role.Defender
-
-						return result.binlog
-					}
-
-					default:
-						throw new Error(`unexpected status code ${result.status}`)
-				}
-			}
-		}
-	}
-
-	reset() {
-		this.state = createState()
-		this.winner = undefined
-	}
-}
-
-export default BinmatGame
+export { createState, Role } from "./createState"
+export type { State } from "./createState"
+export { doMove } from "./doMove"
+export type { Move } from "./doMove"
+export { parseMove } from "./parseMove"
+export { Action, StatusCode, statusCodeMessages } from "./shared"
+export { simulateGame } from "./simulateGame"
