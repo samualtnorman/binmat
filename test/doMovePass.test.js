4ee4a8321fdf410bd34e1132501e7079536ecceb
Samual Norman <samualtnorman@gmail.com>
2022-03-30 18:49:02 +0100

    add tests for `doMovePass()`

---
+++ test/doMovePass.test.js
@@ -0,0 +1,207 @@
+import doMovePass from "../src/doMovePass"
+import { StatusCode } from "../src/shared"
+
+test(`finished game`, () => {
+	/** @type {import("../src/createState").State} */
+	const state = {
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 110,
+		turns: 110,
+		attackerPassedLastTurn: false,
+		defenderPassedLastTurn: false
+	}
+
+	expect(doMovePass(state)).toEqual(StatusCode.MadeMoveOnFinishedGame)
+
+	expect(state).toEqual({
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 110,
+		turns: 110,
+		attackerPassedLastTurn: false,
+		defenderPassedLastTurn: false
+	})
+})
+
+test(`defender win`, () => {
+	/** @type {import("../src/createState").State} */
+	const state = {
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 109,
+		turns: 110,
+		attackerPassedLastTurn: false,
+		defenderPassedLastTurn: false
+	}
+
+	expect(doMovePass(state)).toEqual(StatusCode.DefenderWin)
+
+	expect(state).toEqual({
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 110,
+		turns: 110,
+		attackerPassedLastTurn: true,
+		defenderPassedLastTurn: false
+	})
+})
+
+test(`normal pass`, () => {
+	/** @type {import("../src/createState").State} */
+	const state = {
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 0,
+		turns: 110,
+		attackerPassedLastTurn: false,
+		defenderPassedLastTurn: false
+	}
+
+	expect(doMovePass(state)).toEqual(StatusCode.Ok)
+
+	expect(state).toEqual({
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 1,
+		turns: 110,
+		attackerPassedLastTurn: false,
+		defenderPassedLastTurn: true
+	})
+})
+
+test(`defender pass twice`, () => {
+	/** @type {import("../src/createState").State} */
+	const state = {
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [ `5!` ], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [ `3!`, `3#`, `3%`, `3&`, `3+`, `3^` ],
+		turn: 0,
+		turns: 110,
+		attackerPassedLastTurn: false,
+		defenderPassedLastTurn: true
+	}
+
+	expect(doMovePass(state)).toEqual(StatusCode.Ok)
+
+	expect(state).toEqual({
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [ `3!`, `3^` ], [ `5!` ], [ `3#` ], [ `3%` ], [ `3&` ], [ `3+` ] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 1,
+		turns: 110,
+		attackerPassedLastTurn: false,
+		defenderPassedLastTurn: true
+	})
+})
+
+test(`attacker pass twice`, () => {
+	/** @type {import("../src/createState").State} */
+	const state = {
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [],
+		attackerHand: [ `3!`, `3#` ],
+		defenderHand: [],
+		turn: 1,
+		turns: 110,
+		attackerPassedLastTurn: true,
+		defenderPassedLastTurn: false
+	}
+
+	expect(doMovePass(state)).toEqual(StatusCode.Ok)
+
+	expect(state).toEqual({
+		attackerStacks: [ [], [], [], [], [], [] ],
+		defenderStacks: [
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false },
+			{ cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }, { cards: [], isFaceUp: false }
+		],
+		laneDecks: [ [], [], [], [], [], [] ],
+		laneDiscardPiles: [ [], [], [], [], [], [] ],
+		attackerDeck: [],
+		attackerDiscardPile: [ `3!`, `3#` ],
+		attackerHand: [],
+		defenderHand: [],
+		turn: 2,
+		turns: 110,
+		attackerPassedLastTurn: true,
+		defenderPassedLastTurn: false
+	})
+})
