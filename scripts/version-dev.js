a4e1255a26babdbe1504e8b7617937cb6eeee564
Samual <me@samual.uk>
2024-04-13 20:53:09 +0100

    increment patch number instead of minor for dev builds

--- scripts/version-dev.js
+++ scripts/version-dev.js
@@ -3,7 +3,7 @@ import { spawnSync } from "child_process"
 import * as Semver from "semver"
 import packageConfig from "../package.json" assert { type: "json" }
 
-const incrementedVersion = Semver.inc(packageConfig.version || "0.0.0", "minor")
+const incrementedVersion = Semver.inc(packageConfig.version || "0.0.0", "patch")
 const hash = spawnSync("git", [ "rev-parse", "--short", "HEAD" ], { encoding: "utf8" }).stdout.trim()
 
 spawnSync("pnpm", [ "version", `${incrementedVersion}-${hash}` ], { stdio: "inherit" })
