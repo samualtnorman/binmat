208efeca5d7e319c198f7222676c0226ece76a47
Samual <me@samual.uk>
2024-12-21 20:28:38 +0000

    fix broken script

--- scripts/emit-package-json.js
+++ scripts/emit-package-json.js
@@ -2,7 +2,7 @@
 import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
 import packageJson_ from "../package.json" with { type: "json" }
 
-const { private: _0, devDependencies: _1, pnpm: _2, scripts: _3, engines: { pnpm, ...engines }, ...packageJson } = packageJson_
+const { private: _0, devDependencies: _1, pnpm: _2, engines: { pnpm, ...engines }, ...packageJson } = packageJson_
 
 makeDirectorySync("dist", { recursive: true })
 writeFileSync("dist/package.json", JSON.stringify({ ...packageJson, engines }, undefined, "\t"))
