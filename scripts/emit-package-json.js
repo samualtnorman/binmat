1f9bd59713e0be679d0823bf116fbf585fd0e515
Samual <me@samual.uk>
2024-12-21 19:53:19 +0000

    switch from import `assert`s to `with`

--- scripts/emit-package-json.js
+++ scripts/emit-package-json.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
-import packageJson_ from "../package.json" assert { type: "json" }
+import packageJson_ from "../package.json" with { type: "json" }
 
 const { private: _0, devDependencies: _1, pnpm: _2, scripts: _3, engines: { pnpm, ...engines }, ...packageJson } = packageJson_
 
--- scripts/version-dev.js
+++ scripts/version-dev.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { spawnSync } from "child_process"
 import * as Semver from "semver"
-import packageConfig from "../package.json" assert { type: "json" }
+import packageConfig from "../package.json" with { type: "json" }
 
 const incrementedVersion = Semver.inc(packageConfig.version || "0.0.0", "patch")
 const hash = spawnSync("git", [ "rev-parse", "--short", "HEAD" ], { encoding: "utf8" }).stdout.trim()
