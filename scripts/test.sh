6b3c7f97e64a4dd768466955272b2573d2c84c66
Samual <me@samual.uk>
2024-12-21 21:10:40 +0000

    add `game-scripts` to `test.sh`

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,8 @@ knip
 tsc
 tsc --project src
 vitest run
+scripts/emit-declarations.sh
+tsc --project game-scripts
 rollup --config
+pnpm hsm golf game-scripts/binmat.ts
+pnpm hsm golf game-scripts/binmat_lib.ts
