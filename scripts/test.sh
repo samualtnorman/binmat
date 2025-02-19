18d8a862d6a34e501df04507e5f8390247012984
Samual <me@samual.uk>
2025-02-19 22:57:39 +0000

    fix script path for `emit-declarations.sh` in `test.sh`

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ node_modules/.bin/knip
 node_modules/.bin/tsc
 node_modules/.bin/tsc --project src
 node_modules/.bin/vitest run
-node_modules/.bin/scripts/emit-declarations.sh
+scripts/emit-declarations.sh
 node_modules/.bin/tsc --project game-scripts
 node_modules/.bin/rollup --config
 node_modules/.bin/hsm golf game-scripts/binmat.ts
